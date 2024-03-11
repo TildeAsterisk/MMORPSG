fb8b90767ec2f84a99d771bcf6bdb69b78bf7e18
Zion Duncan <zion@pixel-4a.home>
2024-03-11 22:24:04 +0000

    ...

--- buy_item.php
+++ buy_item.php
@@ -5,6 +5,7 @@ if(!isset($_SESSION['uid'])){
     echo "You must be logged in to view this page!";
 }else{
   if(isset($_POST["buy"])){
+
     $newItem=[
       'name'        => $_POST['name'],
       'description' => $_POST['description'],
