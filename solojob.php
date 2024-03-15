ec1573572df71dc7469dd96a41b82e25ca1fc0fb
Zion Duncan <zion@pixel-4a.home>
2024-03-15 04:25:47 +0000

    Updated solo job logic

--- solojob.php
+++ solojob.php
@@ -4,32 +4,32 @@ include("header.php");
 if(!isset($_SESSION['uid'])){
     echo "You must be logged in to view this page!";
 }else{
-
+  // OUT OF ENERGY
   if ($stats['energy'] < $_POST['energyCost']){
     echo "You don't have enough energy for this job.";
     echo "<br>Wait a while for your energy to regenerate.";
     return;
   }
 
-  //Init job and enemy_stats
-  //Generate enemy stats based on job
+  //Initialise new job
+  //Pull enemy stats from previous job summary post data
   $enemy_stats = [  // Associative Array / Dictionary
     'attack' => $_POST['attack'],
     'defense' => $_POST['defense'],
     'currency' => $_POST['moneyReward']
   ];
+  //generate a random enemy data dict
   $newRandomEnemy=GenerateRandomEnemy($enemy_stats,null);
-
+  //Set other job variables
   $turns=1;//energy modifier?
   $job_energycost=$_POST['energyCost'];
   $job_experiencegained=$_POST['experienceReward'];
+
   //Subtract energy cost of job
   $energycostquery = mysqli_query($mysql,"UPDATE `stats` SET `energy`=`energy`-'".$job_energycost."' WHERE `id`='".$_SESSION['uid']."'") or die(mysqli_error($mysql));
-
   //Attack Effect is = some factor * attack
-  $attack_effect = $stats['attack'];
-  $defense_effect = $enemy_stats['defense'];
-  
+  //$stats['attack'] = $stats['attack'];
+  //$enemy_stats['defense'] = $enemy_stats['defense'];
   $miniStatsProfile=<<<EOD
   <table style='width:100%;text-align:center;'>
   <tr>
@@ -51,34 +51,31 @@ if(!isset($_SESSION['uid'])){
     </tr>
   </table>
   EOD;
-  
-  echo "You prepare for battle.<br><br>";
+  //echo "<center><b>Battle</b></center>";
   echo $miniStatsProfile;
   
   //Generate the battle 
   $weaponTxt = $inventory['weapon']??"bare hands";
 
   //YOUR TURN
-  $damageDealt   = $attack_effect-$defense_effect;
-  if($damageDealt < 0){$damageDealt=0;}
-  $damageBlocked = $attack_effect - $damageDealt;
-  $blockedPercentage=round(($damageBlocked/$attack_effect)*100);
+  $eDamageDealt = $stats['attack']-$enemy_stats['defense'];
+  if($eDamageDealt < 0){$eDamageDealt=0;}
+  $eDamageBlocked = $stats['attack'] - $eDamageDealt;
+  $eBlockedPercentage=round(($eDamageBlocked/$stats['attack'])*100);
   $values = array_values($newRandomEnemy['equipment']);
   $randomEnemyEquipment = $values[array_rand($values)];
-  
   echo "<hr>";
-  echo "You prepare to hit <b>{$newRandomEnemy['name']}</b> with your <b>{$weaponTxt}</b><br>";
+  echo "You prepare to hit <b>{$newRandomEnemy['name']}</b> with your <b>{$weaponTxt}</b>.<br>";
   //echo "[Calculate the chance to hit...]<br>";
-  echo "Your hit lands on <b>{$newRandomEnemy['name']}</b> with a force of <b>{$attack_effect}</b>.<br>";
-  echo "Their <b>{$randomEnemyEquipment->name}</b> soaked up <b>{$blockedPercentage}%</b> of the damage.<br>";
-  echo "You dealt <b>{$damageDealt}</b> damage to the {$newRandomEnemy['name']}.<br>";
+  echo "Your hit lands on <b>{$newRandomEnemy['name']}'s {$randomEnemyEquipment->name}</b> with a force of <b>{$stats['attack']}</b>.<br>";
+  echo "Their <b>{$randomEnemyEquipment->name}</b> soaked up <b>{$eBlockedPercentage}%</b> of the damage.<br>";
+  echo "You dealt <b>{$eDamageDealt}</b> damage to the {$newRandomEnemy['name']}.<br>";
 
-  $attack_effect = $enemy_stats['attack'];
-  $defense_effect = $stats['defense'];
-  $damageDealt   = $attack_effect-$defense_effect;
-  if($damageDealt < 0){$damageDealt=0;}
-  $damageBlocked = $attack_effect - $damageDealt;
-  $blockedPercentage=round(($damageBlocked/$attack_effect)*100);
+  // ENEMIES TURN
+  $eDamageDealt   = $stats['attack']-$enemy_stats['defense'];
+  if($eDamageDealt < 0){$eDamageDealt=0;}
+  $pDamageBlocked = $stats['attack'] - $eDamageDealt;
+  $pBlockedPercentage=round(($pDamageBlocked/$stats['attack'])*100);
   $playerEquipment=[
     'weapon'    => $inventory['weapon'] ??  '{}',
     'head'      => $inventory['head'] ??    '{}',
@@ -92,21 +89,18 @@ if(!isset($_SESSION['uid'])){
   //Enemies turn
   $weaponTxt = $newRandomEnemy['equipment']['weapon']->name ?? "bare hands";
   echo "<hr>";
-  echo "<b>{$newRandomEnemy['name']}</b> prepares to hit you with their <b>{$weaponTxt}</b><br>";
+  echo "<b>{$newRandomEnemy['name']}</b> prepares to hit you with their <b>{$weaponTxt}</b>.<br>";
   //echo "[Calculate the chance to hit...]<br>";
-  echo "<b>{$newRandomEnemy['name']}</b> strikes you with a force of <b>{$attack_effect}</b>.<br>";
-  echo "Your <b>{$randomPlayerEquipmentTxt}</b> soaked up <b>{$blockedPercentage}%</b> of the damage.<br>";
-  echo "<b>{$newRandomEnemy['name']}</b> dealt <b>{$damageDealt}</b> damage to you.<br>";
+  echo "<b>{$newRandomEnemy['name']}</b> strikes your <b>{$randomPlayerEquipmentTxt}</b> with a force of <b>{$stats['attack']}</b>.<br>";
+  echo "Your <b>{$randomPlayerEquipmentTxt}</b> soaked up <b>{$pBlockedPercentage}%</b> of the damage.<br>";
+  echo "<b>{$newRandomEnemy['name']}</b> dealt <b>{$eDamageDealt}</b> damage to you.<br>";
+  ECHO "<hr>";
 
-  $attack_effect = $stats['attack'];
-  $defense_effect = $enemy_stats['defense'];
-  if($attack_effect > $defense_effect){
-      $ratio = ($attack_effect - $defense_effect)/$attack_effect * $turns;
+  if($stats['attack'] > $enemy_stats['defense']){
+      $ratio = ($stats['attack'] - $enemy_stats['defense'])/$stats['attack'] * $turns;
       $ratio = min($ratio,1);
       $gold_stolen = (int)floor($ratio/2 * $enemy_stats['currency']);
-      echo "<br>You defeated the enemy!<br> You stole " . $gold_stolen . " gold!";
-
-      echo "<br><br>You completed the Job.<br>";
+      echo "You defeated the enemy!<br> You stole " . $gold_stolen . " gold!";
       echo "<center><h3>Completed the Job successfully!</h3></center>";
 
       //Add gained experience
@@ -118,10 +112,10 @@ if(!isset($_SESSION['uid'])){
       
       //Enter battle log into db
       $battle3 = mysqli_query($mysql,"INSERT INTO `logs` (`attacker`,`defender`,`attacker_damage`,`defender_damage`,`currency`,`food`,`time`) 
-                              VALUES ('".$_SESSION['uid']."','"."0"."','".$attack_effect."','".$defense_effect."','".$gold_stolen."','0','".time()."')") or die(mysqli_error($mysql));
+                              VALUES ('".$_SESSION['uid']."','"."0"."','".$stats['attack']."','".$enemy_stats['defense']."','".$gold_stolen."','0','".time()."')") or die(mysqli_error($mysql));
       //$stats['turns'] -= $turns;
   }else{
-    echo "<br><br>The enemies defenses were too strong...<br>";
+    echo "<br>The enemies defenses were too strong...<br>";
     echo "<center><h3>You failed the Job.</h3></center>";
     //MONEY/ENERGY penalty?
   }
