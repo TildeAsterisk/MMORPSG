20028c74a14c707eed8bd6d7b89e09436401f5e5
Zion Duncan <zion@Godfreys-MacBook-Pro.local>
2024-03-23 21:55:31 +0000

    Updating the settlements page. Need to implement update to DB for workers form

--- main.php
+++ main.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include_once("header.php");
+include("settlement.php");
 
 // Check if player is logged in
 //$_SESSION['uid'] = null;
@@ -11,23 +12,26 @@ else{
   //Player is logged in, show main page
   ?>
 <center><h1>Control Center</h1></center>
-<pre>
-Settlements:
-  Stats: Location, Conditions, Population, Food, Resources
-  - Activities:
-    * List of current jobs (foragers, builders, attackers)
-  - Expeditions
-</pre>
   <?php
-  include("settlement.php");
-  //Init Settlement Obj
-  $pSettlement = new Settlement();
-  $pSettlement->InitSettlement();
+  //Find settlement insql db
+  $getPlayerStlmntQuery = mysqli_query($mysql,"SELECT * FROM `settlement` WHERE `id`='".$_SESSION['uid']."'") or die(mysqli_error($mysql));
+  $pSettlement = [ 'data' => (object)mysqli_fetch_assoc($getPlayerStlmntQuery)];
+  if($pSettlement ==null){
+    //Init Settlement Obj
+    $pSettlement = new Settlement();
+    $pSettlement->InitSettlement($mysql);
+  }
+  //var_dump($pSettlement['data']);
   /*echo "<hr>Settlement Initialized.<br>";
   echo "\$data: ",var_dump($pSettlement->data),"<hr>";
   */
-  
-  
+  //$pSettlement->UpdateSettlement();
+  echo "<center><b>Total Population: {$pSettlement['data']->population}</b><br>";
+  echo SettlementJobBoardHTMl($pSettlement),"<br>";
+  //display income for food, resources
+  echo "Income:<br>+1 {$food_symbol}/day<br>+1 {$materials_symbol}/day<br>";
+  echo "<br>Buildings:";
+  echo "</center>";
 
 }
 //include("update_stats.php");
--- settlement.php
+++ settlement.php
@@ -1,23 +1,103 @@
 <?php 
-
+const SETTLEMENT_TAG_NAME       = 'Name';
+const SETTLEMENT_TAG_LOCATION   = "Location";
+const SETTLEMENT_TAG_POPULATION = "Population";
+const SETTLEMENT_TAG_JOBS       = "Jobs";
+const SETTLEMENT_TAG_FOOD       = "Food";
+const SETTLEMENT_TAG_MATERIALS  = "Materials";
 class Settlement{
-  //Set constants
   public $data;
   
-  function InitSettlement($data = [
-    'Name'        => "Settlement Name",
-    'Location'    => "Local Area",
-    'Population'  => 0,
-    'Food'        => 0,
-    'Materials'   => 0])
+  function InitSettlement($mysql,
+    $data = [
+      SETTLEMENT_TAG_NAME        => "Settlement Name",
+      SETTLEMENT_TAG_LOCATION    => "Local Area",
+      SETTLEMENT_TAG_POPULATION  => 10,
+      SETTLEMENT_TAG_JOBS        => [
+        'Foraging'        => 1, //[JobName->NumWorkers]
+        'Combat' => 0
+      ],
+      SETTLEMENT_TAG_FOOD        => 0,
+      SETTLEMENT_TAG_MATERIALS   => 0 ])
   {
     $this->data = $data;
-    /*
-    self::$data=$data;
-    echo "Settlement Initialized.<br>";
-    echo "\$data: ",var_dump(self::$data);
-    */
+    //var_dump($data);
+    $data=(object)$data;
+    //Update SQL databse with new settlement
+    $updateQuery="INSERT INTO `settlement` (`ownerid`, `name`,`population`,`food`,`materials`,`jobs`) VALUES (0,'$data->Name','$data->Population','$data->Food','$data->Materials',null)";
+    var_dump($updateQuery);
+    $sqlexec = mysqli_query($mysql, $updateQuery);
   }
+
+  function GetTotalEmployed(){
+    $totalEmployed = 0;
+    foreach ($this->data[SETTLEMENT_TAG_JOBS] as $key => $value) {
+      $totalEmployed+=$value[1];
+    }
+    return $totalEmployed;
+  }
+
+  function UpdateSettlement(){
+    //Foragers generate 3 food and materials per day
+    $numForaging=$this->data[SETTLEMENT_TAG_JOBS]['Foraging'];
+    $foodIncome = $numForaging * 3;
+    $matIncome = $numForaging * 3;
+  }
+  
+  //Select number of pop. to assign jobs
+  function AssignJobs($numPop, $jobName){
+    $totalEmployed = $this->GetTotalEmployed();
+    $totalUnemployed = $this->data[SETTLEMENT_TAG_POPULATION] - $totalEmployed;
+    //check if num of pop is valid
+    if($numPop > $this->data[SETTLEMENT_TAG_POPULATION]){
+      echo "You don't even have that many people in your settlement.";
+    }
+    // Check if $numPop is greater than those unemployed
+    if($numPop > $totalUnemployed){
+      echo "You don't have that many people to assign roles to.";
+    }
+
+    $stlmntJobs = $this->data[SETTLEMENT_TAG_JOBS];
+    $stlmntJobs[$jobName]->$numPop;
+    echo "$numPop workers assigned to the role of $jobName";
+  }
+}
+
+//SETTLEMENT UI FUNCTIONS
+function SettlementJobBoardHTML($stlmnt){
+  //var_dump(json_decode($stlmnt['data']->jobs));
+  $stlmntJobs = json_decode($stlmnt['data']->jobs) ?? [];
+  //Get list of jobs
+  $jobsTableRowsHTMl = "";
+  //var_dump($stlmntJobs);
+  foreach($stlmntJobs as $job => $numWrkrs){
+    //var_dump($numWrkrs);
+    $jobsTableRowsHTMl .= <<<EOD
+    <tr>
+        <td>{$numWrkrs} {$job}</td>
+        <form action="enroll_units.php" method="post">
+        <td>
+        <input style="width:100%;" type="range" name="numWorkers" value="$numWrkrs" min="0" max="{$stlmnt['data']->population}" oninput="rangeValue.innerText = this.value" />
+        </td>
+        <td><output id="rangeValue">{$numWrkrs}</output></td>
+        <td>
+        <input style="width:100%;" type="submit" name="buy" value="Enroll" />
+        </td>
+        </form>
+        </tr>
+    EOD;
+  }
+
+  $jobsTableHTML = <<<EOD
+  <center><table id='inventoryTable'>
+    <tr>
+      <td>Roles:</td>
+      <td></td>
+    </tr>
+    $jobsTableRowsHTMl
+  </table></center>
+  EOD;
+  return $jobsTableHTML;
 }
 
 
