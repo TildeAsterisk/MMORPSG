6cf59fea883736f79c59b60ab65cbd2a9029619d
Zion Duncan <zion@Godfreys-MBP.cust.communityfibre.co.uk>
2024-04-09 19:18:34 +0100

    Ability to destroy tiles #6 and #4

--- cell_select.php
+++ cell_select.php
@@ -94,8 +94,21 @@ else{
       echo "Co-oridinates: X:$cellSelected[x],Y:$cellSelected[y]<br>";
       echo "<br>Full Selected Cell Data:<br>";
       var_dump($selectedCellData);
+      var_dump($_POST);
       echo "<br><br>";
-      echo "<button disabled='true'>Upgrade</button><button disabled='true'>Destroy</button> <br>";
+      echo "<form method='post'> <button disabled='true'>Upgrade</button><input type='submit' name='destroyButton' value='Destroy'/><input type='hidden' name='cell-data' value='{$_POST['cell-data']}' /></form><a href='main.php'><button>Back to Home Grid</button></a><br>";
+    }
+
+    //Manage Cell Actions
+    if(isset($_POST["destroyButton"])){
+      //Destroy current cell
+      //update in sql
+      unset($gridData["$cellSelected[x],$cellSelected[y]"]);
+      //update grid cell data to SQL DB
+      $gridDataEncoded = json_encode($gridData);
+      $updateGridDataQuery = "UPDATE `settlement` SET `grid-data`='$gridDataEncoded' WHERE `id`='".$_SESSION['uid']."'";
+      mysqli_query($mysql,$updateGridDataQuery) or die(mysqli_error($mysql));
+      echo "Destroyed cell X:$cellSelected[x],Y:$cellSelected[y].";
     }
 
   }
