2be96a4d533186c4282036335de5e84bd8d9b14d
Zion Duncan <zion@pixel-4a.home>
2024-04-17 22:26:35 +0100

    Adding placeholder graphics for each tile type #9 Centre, Storage, Barracks, Turret, Farm

--- cell_select.php
+++ cell_select.php
@@ -26,6 +26,7 @@ else{
 
     //if doesn't already have data
     if(!$selectedCellData){
+      echo "<center><h2>Build</h2></center>";
 
       $emptyCellActionsHTML = <<<EOD
       <form method="post"> 
@@ -35,9 +36,9 @@ else{
           <option value="Farm">Farm</option>
           <option value="Barracks">Barracks</option>
           <option value="Turret">Turret</option>
+          <option value="Storehouse">Storehouse</option>
         </select>
         <input type="submit" name="buildButton" value="Build"/><br>
-        <input type="submit" name="refresh" value="Manage Structure"/> 
         <!--input type="submit" name="backButton" value="Back To Base Grid"/-->
         <input type="hidden" name="cell-data" value="{$_POST["cell-data"]}" /> 
       </form> 
@@ -73,6 +74,12 @@ else{
               "cellType" => "CT-Turret"
             ];
             break;
+          case "Storehouse":
+            //Add new cell data
+            $gridData["$cellSelected[x],$cellSelected[y]"] = [
+              "cellType" => "CT-Storage"
+            ];
+            break;
           default:
             //Add new cell data
             $gridData["$cellSelected[x],$cellSelected[y]"] = [
@@ -86,19 +93,33 @@ else{
         $updateGridDataQuery = "UPDATE `settlement` SET `grid-data`='$gridDataEncoded' WHERE `id`='".$_SESSION['uid']."'";
         mysqli_query($mysql,$updateGridDataQuery) or die(mysqli_error($mysql));
         echo "Updated cell X:$cellSelected[x],Y:$cellSelected[y].";
+        //set selectedCellData so that screen changes to show manage tile building
+        //$selectedCellData=$gridData["$cellSelected[x],$cellSelected[y]"];
         
       }
 
     }
     else{
       //Cell already filled
-      echo "<center><h2>".$selectedCellData->cellType."</h2></center>";
+      //Init celltypes-imgpaths
+      $cellTypes=[
+        "CT-Farm"     => ["imgPath"=>"Graphics/farmtile.png"],
+        "CT-Centre"   => ["imgPath"=>"Graphics/hometile.png"],
+        "CT-Storage"  => ["imgPath"=>"Graphics/storagetile.png"],
+        "CT-Barracks" => ["imgPath"=>"Graphics/barrackstile.png"]
+      ];
+      $tileImgPath=$cellTypes[$selectedCellData->cellType]["imgPath"] ?? "";
+      echo "<center><h2>".$selectedCellData->cellType."</h2>";
+      echo "<img style='width:25%;' src='".$tileImgPath."'/>";
+      echo "<br><br>";
+      echo "<form method='post'> <button disabled='true'>Upgrade</button><input type='submit' name='destroyButton' value='Destroy'/><input type='hidden' name='cell-data' value='{$_POST['cell-data']}' /></form><a href='main.php'><button>Back to Home Grid</button></a><br></center>";
       echo "Co-oridinates: X:$cellSelected[x],Y:$cellSelected[y]<br>";
       echo "<br>Full Selected Cell Data:<br>";
       var_dump($selectedCellData);
+      echo "<br>";
       var_dump($_POST);
-      echo "<br><br>";
-      echo "<form method='post'> <button disabled='true'>Upgrade</button><input type='submit' name='destroyButton' value='Destroy'/><input type='hidden' name='cell-data' value='{$_POST['cell-data']}' /></form><a href='main.php'><button>Back to Home Grid</button></a><br>";
+      echo "<br><hr>";
+
     }
 
     //Manage Cell Actions
